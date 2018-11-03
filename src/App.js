933b454f128088b7a215332a93847c478bf25b16
Shubham Vijay Vargiy <svijay1692@gmail.com>
2018-11-03 23:26:10 +0530

    Add nested object option functionlity

--- src/App.js
+++ src/App.js
@@ -1,41 +1,94 @@
-import React, { Component } from 'react';
-import Select from './Select';
-import './App.css';
+import React, { Component } from 'react'
+import Select from './Select'
+import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      viewCount : {}
+      viewCount: null,
+      menuName: 'baisc'
     }
   }
-  onSelect(selecteOptions, fieldName) {
+  onSelect (selecteOptions, fieldName) {
     this.setState({
       [fieldName]: selecteOptions
-    });
+    })
   }
-  render() {
-    const {viewCount} = this.state;
-    const viewOptions = [{ label: 12, value: 12 }, { label: 24, value: 24 }, { label: 48, value: 48 }, { label: 96, value: 96 }];
+
+  onMenuSelect (menuName) {
+    this.setState({
+      menuName: menuName
+    })
+  }
+  render () {
+    const { selected, menuName } = this.state
+    const viewOptions = [{ label: 12, value: 12 }, { label: 24, value: 24 }, { label: 48, value: 48 }, { label: 96, value: 96 }]
+    const nestedViewOptions = [{
+      label: 12,
+      value: { name: 12 }
+    }, { label: 24, value: { name: 24 } }, { label: 48, value: { name: 48 } }, { label: 96, value: { name: 96 } }]
     const viewCountSelectProps = {
       options: {
         valueFieldName: 'value',
         labelFiledName: 'label',
-        showPlaceHolder: true,
+        showPlaceHolder: true
       },
       selectOptions: viewOptions,
-      selected: viewCount && viewCount.value ? viewCount.value : '',
-      name: 'viewCount',
+      selected,
+      name: 'selected',
       required: true,
       disabled: false,
+      style: {
+        textAlign: 'center'
+      },
       onSelect: (selecteOptions, fieldName) => this.onSelect(selecteOptions, fieldName)
     }
+    const nestedOptionSelectProps = {
+      options: {
+        valueFieldName: 'value.name',
+        labelFiledName: 'label',
+        showPlaceHolder: true
+      },
+      selectOptions: nestedViewOptions,
+      selected,
+      name: 'selected',
+      required: true,
+      disabled: false,
+      style: {
+        textAlign: 'center'
+      },
+      onSelect: (selecteOptions, fieldName) => this.onSelect(selecteOptions, fieldName)
+    }
+    console.log('menuName', menuName)
     return (
-      <div className="App">
-        <Select {...viewCountSelectProps} />
+      <div className='App'>
+        <div className='app-container'>
+          <div className='vertical-menu'>
+            <div className={`btn ${menuName === 'baisc' ? 'active' : ''}`} onClick={() => this.onMenuSelect('baisc')}>Baisc Select</div>
+            <div className={`btn ${menuName === 'nestedObjectOption' ? 'active' : ''}`} onClick={() => this.onMenuSelect('nestedObjectOption')}>Nested Object Option Select</div>
+            {/* <div className="btn ">Link 2</div>
+            <div className="btn ">Link 3</div>
+            <div className="btn ">Link 4</div> */}
+          </div>
+          <div className='vertical-container'>
+            {menuName === 'baisc' && <div>
+              <h1> Basic Select </h1>
+              <div className='select-example-container'>
+                <Select {...viewCountSelectProps} />
+              </div>
+            </div>}
+            {menuName === 'nestedObjectOption' && <div>
+              <h1> Nested Object Option Select </h1>
+              <div className='select-example-container'>
+                <Select {...nestedOptionSelectProps} />
+              </div>
+            </div>}
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Select/index.js
+++ src/Select/index.js
@@ -1,8 +1,8 @@
 
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Select.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Select.css'
 
 const defaultOptions = {
   valueFieldName: 'value',
@@ -11,8 +11,11 @@ const defaultOptions = {
   placeHoldder: 'Select',
   multi: true
 }
-class Select extends React.Component {
 
+const defaultStyle = {
+  width: '100%'
+}
+class Select extends React.Component {
   static propTypes = {
     onSelect: PropTypes.func,
     selectOptions: PropTypes.array,
@@ -21,7 +24,8 @@ class Select extends React.Component {
     selected: PropTypes.any,
     disabled: PropTypes.bool,
     disabledClassName: PropTypes.string,
-    required: PropTypes.bool
+    required: PropTypes.bool,
+    style: PropTypes.object
   };
   static defaultProps = {
     onSelect: () => { },
@@ -33,57 +37,78 @@ class Select extends React.Component {
     required: false
   };
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       openDropdown: false
     }
   }
 
-  componentDidMount() {
-    document.addEventListener('mousedown', (event) => { this.handleClickOutside(event) });
+  componentDidMount () {
+    document.addEventListener('mousedown', (event) => { this.handleClickOutside(event) })
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', (event) => { this.handleClickOutside(event) });
+  componentWillUnmount () {
+    document.removeEventListener('mousedown', (event) => { this.handleClickOutside(event) })
   }
 
-  handleClickOutside(event) {
-    if (this.dropdownRef && !this.dropdownRef.contains(event.target))
-      this.setState({ openDropdown: false });
+  handleClickOutside (event) {
+    if (this.dropdownRef && !this.dropdownRef.contains(event.target)) { this.setState({ openDropdown: false }) }
   }
 
-  render() {
+  acessJsonByPath (path, json) {
+    return path.split('.').reduce((o, k) => {
+      return o && o[k]
+    }, json)
+  }
 
-    const { openDropdown } = this.state;
-    const { selectOptions, options, onSelect, name, selected, disabled, disabledClassName, required } = this.props;
-    const realOPtions = {...defaultOptions, ...options};
-    const { valueFieldName, labelFiledName, showPlaceHolder, placeHoldder } = realOPtions;
-    const selectObject = selectOptions.find((option) => option[valueFieldName] === selected) || { [valueFieldName]: '', [labelFiledName]: placeHoldder };
+  selectedValue (valueFieldName, selected) {
+    return this.acessJsonByPath(valueFieldName, selected) || selected
+  }
+
+  blankSelectedValue (valueFieldName, labelFiledName, placeHoldder) {
+    let blankObj = {}
+    valueFieldName.split('.').reverse().forEach((p, i) => {
+      i === 0 ? blankObj[p] = '' : blankObj = { [p]: blankObj }
+    })
+    blankObj[labelFiledName] = placeHoldder
+    console.log(blankObj)
+    return blankObj
+  }
+
+  render () {
+    const { openDropdown } = this.state
+    const { selectOptions, options, onSelect, name, selected, disabled, disabledClassName, required, style } = this.props
+    const realOPtions = { ...defaultOptions, ...options }
+    const customStyle = { ...defaultStyle, ...style }
+    const { valueFieldName, labelFiledName, showPlaceHolder, placeHoldder } = realOPtions
+    const blankValue = this.blankSelectedValue(valueFieldName, labelFiledName, placeHoldder)
+    const selectObject = selectOptions.find((option) => this.acessJsonByPath(valueFieldName, option) === this.selectedValue(valueFieldName, selected)) || blankValue
+    console.log('selectObject', selectObject, selected)
     return (
       <div ref={node => {
-        this.dropdownRef = node;
+        this.dropdownRef = node
       }} className={`react-custom-select ${openDropdown ? 'open' : ''}`} onClick={() => {
         if (!disabled) {
           this.setState({
             openDropdown: !openDropdown
-          });
+          })
         }
-      }}>
+      }} style={customStyle}>
         <select style={{
           display: 'none'
-        }} value={selected} required={required} onChange={()=>{}}>
-          {selectOptions.map(option => (<option key={option[valueFieldName]} value={option[valueFieldName]}>{option[labelFiledName]}</option>))}
+        }} value={selected || ''} required={required} onChange={() => { }}>
+          {selectOptions.map(option => (<option key={this.acessJsonByPath(valueFieldName, option)} value={this.acessJsonByPath(valueFieldName, option)}>{this.acessJsonByPath(labelFiledName, option)}</option>))}
         </select>
-        <span className={`current text-capitalize ${disabled ? disabledClassName || 'disabled' : ''}`}>{selectObject[labelFiledName]}</span><ul className="list">
-          {showPlaceHolder && <li data-value="value" className={`option focus  text-capitalize ${selectObject[valueFieldName] === '' ? 'selected' : ''}`} onClick={() => {
-            onSelect({ [valueFieldName]: '', [labelFiledName]: placeHoldder }, name);
-          }}>{placeHoldder}</li>}
-          {selectOptions.map(option => (<li key={option[valueFieldName]} data-value="value" className={`option focus  text-capitalize ${selected === option[valueFieldName] ? 'selected' : ''}`} onClick={() => {
-            onSelect(option, name);
-          }}>{option[labelFiledName]}</li>))}</ul></div>
-    );
+        <span className={`current text-capitalize ${disabled ? disabledClassName || 'disabled' : ''}`}>{selectObject[labelFiledName]}</span><ul className='list' style={customStyle}>
+          {showPlaceHolder && <li data-value='value' className={`option focus  text-capitalize ${this.acessJsonByPath(valueFieldName, selectObject) === '' ? 'selected' : ''}`} onClick={() => {
+            onSelect(blankValue, name)
+          }} style={customStyle}>{placeHoldder}</li>}
+          {selectOptions.map(option => (<li key={this.acessJsonByPath(valueFieldName, option)} data-value='value' className={`option focus  text-capitalize ${this.selectedValue(valueFieldName, selected) === this.acessJsonByPath(valueFieldName, option) ? 'selected' : ''}`} onClick={() => {
+            onSelect(option, name)
+          }} style={customStyle}>{this.acessJsonByPath(labelFiledName, option)}</li>))}</ul></div>
+    )
   }
 }
 
-export default Select;
+export default Select
